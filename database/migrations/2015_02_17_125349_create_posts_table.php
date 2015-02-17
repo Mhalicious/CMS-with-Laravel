2a73be7f80f47682269a0744d07c7ea0f2ffc5e5
Matthias Harreither <m_harreither@hotmail.com>
2015-02-17 14:00:33 +0100

    CMS with Laravel
    first little tiny Laravel steps

---
+++ database/migrations/2015_02_17_125349_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function($table))
+		{
+			$table->increments('id');
+			$table->string('title',200);
+			$table->string('slug',400);//looks-like-this, besser für URL
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
